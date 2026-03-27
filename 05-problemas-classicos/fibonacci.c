1d224a7fd05d866d3ff2941b468df25ad38b19ce
PabloArthurNasc <pabloarthurnasc@gmail.com>
2026-03-27 10:59:21 -0300

    fibonacci.c
    Programa que exibe os termos da sequência de Fibonacci com base na quantidade informada pelo usuário.

--- 05-problemas-classicos/fibonacci.c
+++ 05-problemas-classicos/fibonacci.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
 
 int main() {
-    int n, i;
+    // Variável para armazenar a quantidade de termos desejados
+    int n;
+
+    // Variável de controle do loop
+    int i;
+
+    // Primeiros valores da sequência de Fibonacci
     int primeiro = 0, segundo = 1, proximo;
 
+    // Solicita ao usuário quantos termos ele quer ver
     printf("Quantos termos da sequência de Fibonacci você quer ver? ");
     scanf("%d", &n);
 
+    // Exibe um título antes da sequência
     printf("Sequência de Fibonacci:\n");
 
+    // Loop que vai repetir 'n' vezes
     for (i = 0; i < n; i++) {
+
+        // Exibe o valor atual da sequência
         printf("%d ", primeiro);
 
+        // Calcula o próximo termo somando os dois anteriores
         proximo = primeiro + segundo;
+
+        // Atualiza os valores:
+        // o "primeiro" passa a ser o "segundo"
         primeiro = segundo;
+
+        // o "segundo" passa a ser o "próximo"
         segundo = proximo;
     }
 
